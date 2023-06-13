ef649c3ce236d90b4c782d48fa04beb46b0b1812
Pacifique Rubasha <Pacifiquer@frci.local>
2023-06-13 13:29:47 +0400

    Test iOS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { usePWAInstall } from 'react-use-pwa-install'
 
 function App() {
 
@@ -22,30 +21,34 @@ function App() {
     return () => window.removeEventListener("transitionend", handler);
   }, []);
 
-  const onClick = evt => {
-    evt.preventDefault();
+  const handleShowPWA = e => {
+    e.preventDefault();
     if (!promptInstall) {
-      alert("NO PROMPT DATA")
       return;
     }
     promptInstall.prompt();
   };
   if (!supportsPWA) {
-    alert("NOT SUPPORTED")
     return null;
   }
 
   return (
     <div className="App">
-         <button
+        <h1>Lorem Ipsum Dolor</h1>
+        <p>
+        But don't worry, we have a fix for that! We can add a custom popup which will indicate that our app can be added to home screen.
+
+        You are free to design that popup as you wish, our example is shown below. The hard part is to display it only in Safari and not in standalone mode (when the app is already added to home screen). You can check if your app is in standalone mode
+        </p>
+        {supportsPWA && <button
             className="link-button"
             id="setup_button"
             aria-label="Install app"
             title="Install app"
-            onClick={onClick}
+            onClick={handleShowPWA}
           >
             Install
-          </button>
+          </button>}
     </div>
   );
 }
