c7fea7c71bffb0f93f954220751c1a6d37d7166a
Pacifique Rubasha <Pacifiquer@frci.local>
2023-06-13 12:44:26 +0400

    Test iOS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { usePWAInstall } from 'react-use-pwa-install'
@@ -6,13 +6,46 @@ import { usePWAInstall } from 'react-use-pwa-install'
 function App() {
 
 
-	const install = usePWAInstall()
+	// const install = usePWAInstall()
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = e => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const onClick = evt => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      alert("NO PROMPT DATA")
+      return;
+    }
+    promptInstall.prompt();
+  };
+  if (!supportsPWA) {
+    alert("NOT SUPPORTED")
+    return null;
+  }
 
   return (
     <div className="App">
-        <button type="button" onClick={install}>
-          Install Appx
-        </button>
+         <button
+            className="link-button"
+            id="setup_button"
+            aria-label="Install app"
+            title="Install app"
+            onClick={onClick}
+          >
+            Install
+          </button>
     </div>
   );
 }
