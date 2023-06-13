06418f9a324ed2566a65df0d16804f4eaa32accc
Pacifique Rubasha <Pacifiquer@frci.local>
2023-06-13 10:14:00 +0400

    TEST 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { usePWAInstall } from 'react-use-pwa-install'
 
 function App() {
+
+
+	const install = usePWAInstall()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button type="button" onClick={install}>
+          Install Appx
+        </button>
     </div>
   );
 }
